dbfe26b840174f483ced566c7ad8ae839966adbc
Ishika Goyal <69114593+Ishika-go@users.noreply.github.com>
2024-04-29 01:09:26 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,4 @@
-// import { useState, useEffect } from "react";
 
-// export default function App() {
-//   console.log("line no. 5");
-//   const [isRunning, setIsRunning] = useState(false);
-//   const [elapsedTime, setElapsedTime] = useState(0);
-//   const formatTime = (time) => {
-//     let minute = Math.floor(time / 60);
-//     let second = time % 60;
-//     return `${minute} : ${second}`;
-//   };
-//   function handleOnclick() {
-//     setIsRunning((prev) => !prev);
-//   }
-//   const handleReset = () => {
-//     setIsRunning(false);
-//     setElapsedTime(0);
-//   };
-//   useEffect(() => {
-//     console.log("line no. 21");
-//     let intervalId;
-//     if (isRunning) {
-//       intervalId = setInterval(() => {
-//         setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
-//       }, 1000);
-//     } else {
-//       console.log("line no. 28");
-//       clearInterval(intervalId);
-//     }
-//     console.log("line no. 31");
-//     return () => {
-//       clearInterval(intervalId);
-//     };
-//   }, [isRunning]);
-
-//   console.log("line no. 37");
-//   return (
-//     <div>
-//       <h2>Stopwatch</h2>
-//       <p>Time: {formatTime(elapsedTime)} </p>
-//       <button onClick={handleOnclick}>{isRunning ? "stop" : "start"}</button>
-
-//       <button onClick={handleReset}>Reset</button>
-//     </div>
-//   );
-// }
 import { useEffect, useState } from 'react'
 
 
